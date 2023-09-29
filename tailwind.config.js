5a86678e34ca01bb5857ba224ad8b7e244cf38fe
ameerah-codes <81390638+ameerah-codes@users.noreply.github.com>
2023-09-29 20:23:59 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      brightRed: 'hsl(12, 88%, 59%)',
+      brightRedLight: 'hsl(12, 88%, 69%)',
+      brightRedSupLight: 'hsl(12, 88%, 95%)',
+      darkBlue: 'hsl(228, 39%, 23%)',
+      darkGrayishBlue: 'hsl(227, 12%, 61%)',
+      veryDarkBlue: 'hsl(223, 12%, 13%)',
+      veryPaleRed: 'hsl(13, 100%, 96%)',
+      veryLightGray: 'hsl(0, 0%, 98%)',
+      white: 'hsl(0, 0%, 100%)',
+      gray: 'hsl(0, 0%, 62%)',
+      bluishGray: 'hsl(228, 23%, 20%)',
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
